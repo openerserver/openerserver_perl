2f1cdf1f8b8280a5ea85db7fd100ff28a0687163
Openerserver <openercn@gmail.com>
2016-12-26 20:54:13 +0800

    Update create_letsencrypt_pem.sh

--- util/create_letsencrypt_pem.sh
+++ util/create_letsencrypt_pem.sh
@@ -8,18 +8,23 @@ echo "Usage: $(basename $0) <domain> <email>"
 exit 11
 fi
 
+if [-e "certbot-auto"]; then
+./certbot-auto certonly --standalone -d $DOMAIN -m $EMAIL -n --agree-tos --debug
+else
 wget https://dl.eff.org/certbot-auto
 chmod a+x certbot-auto
-./certbot-auto certonly --standalone -d $DOMAIN -m $EMAIL -n --agree-tos
+./certbot-auto certonly --standalone -d $DOMAIN -m $EMAIL -n --agree-tos --debug
+fi
 
 cat /etc/letsencrypt/keys/0000_key-certbot.pem >> $DOMAIN.pem
 cat /etc/letsencrypt/live/$DOMAIN/cert.pem > $DOMAIN.pem
 cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem >> $DOMAIN.pem
 if [-d "../pems"]; then
-
+mv $DOMAIN.pem ../cert_pems/
 else
 mkdir "../cert_pems"
+mv $DOMAIN.pem ../cert_pems/
 fi
 
-mv $DOMAIN.pem ../cert_pems/
+
 
